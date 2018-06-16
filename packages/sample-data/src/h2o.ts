aab63538848f4ec7e326e6f6dcd590cacd0b54a8
Alessandro Genova <ales.genova@gmail.com>
2018-06-16 11:56:28 -0400

    fix: Add a new package with sample cjson data

---
+++ packages/sample-data/src/h2o.ts
@@ -0,0 +1,45 @@
+import { IChemJson } from '@openchemistry/types';
+export { Water };
+
+const Water: IChemJson = {
+  "atoms" : 
+  {
+    "coords" : 
+    {
+      "3d" : 
+      [
+        0.0,
+        0.0,
+        -0.26192472620000,
+        0.0,
+        0.77162768440000,
+        0.34261631290000,
+        0.0,
+        -0.77162768440000,
+        0.34261631290000
+      ]
+    },
+    "elements" : 
+    {
+      "number" : [ 8, 1, 1 ],
+      "symbol" : [ "O", "H", "H"]
+    }
+  },
+  "bonds" : 
+  {
+    "connections" : 
+    {
+      "index" : 
+      [
+        0,
+        1,
+        0,
+        2
+      ]
+    },
+    "order" : [ 1, 1 ]
+  },
+  "chemical json" : 0
+};
+
+
