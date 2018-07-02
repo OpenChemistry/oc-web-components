5963cb27c09882fb8555cda7226f5d32a9152a54
Alessandro Genova <alessandro.genova@kitware.com>
2018-07-02 13:33:42 -0400

    feat(energy-plot) First version of the energy-plot component

---
+++ packages/energy-plot/stencil.config.js
@@ -0,0 +1,17 @@
+exports.config = {
+  namespace: 'energy-plot',
+  outputTargets:[
+    { 
+      type: 'dist' 
+    },
+    { 
+      type: 'www',
+      serviceWorker: false
+    }
+  ]
+};
+
+exports.devServer = {
+  root: 'www',
+  watchGlob: '**/**'
+}
