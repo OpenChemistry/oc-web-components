b3279dec70c7ae2abd532dcf8a898d94adb1eeac
Alessandro Genova <alessandro.genova@kitware.com>
2018-07-05 12:41:31 -0400

    Move reducers and selectors to @openchemistry/redux
    It will allow code sharing between the SPA and the Notebook

---
+++ packages/redux/src/index.js
@@ -0,0 +1,21 @@
+import molecules  from './ducks/molecules';
+import calculations  from './ducks/calculations';
+import users  from './ducks/users';
+import girder  from './ducks/girder';
+import app  from './ducks/app';
+import cumulus  from './ducks/cumulus';
+import nersc  from './ducks/nersc';
+import jupyterlab from './ducks/jupyterlab';
+
+export const reducers = {
+  molecules,
+  calculations,
+  users,
+  girder,
+  app,
+  cumulus,
+  nersc,
+  jupyterlab
+};
+
+export { default as selectors } from './selectors';
