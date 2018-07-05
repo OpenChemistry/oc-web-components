f9e108de58a60479aec09120b872842bfb3df2df
Alessandro Genova <alessandro.genova@kitware.com>
2018-07-05 13:15:19 -0400

    Move rest calls to @openchemistry/rest

---
+++ packages/rest/rollup.config.js
@@ -0,0 +1,16 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import pkg from './package.json';
+
+export default [
+  {
+    input: 'src/index.js',
+    output: [{
+      file: pkg.module, format: 'es'
+    }],
+    plugins: [
+      resolve(),
+      commonjs()
+    ],
+  }
+];
