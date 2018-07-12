95253d9f346fa9f18212e274c93d2d67fda64a96
Alessandro Genova <alessandro.genova@kitware.com>
2018-07-12 11:42:11 -0400

    Improve life cycle and enable normal mode animation

--- packages/molecule-vtkjs/src/components/molecule-vtkjs/molecule-vtkjs.tsx
+++ packages/molecule-vtkjs/src/components/molecule-vtkjs/molecule-vtkjs.tsx
@@ -4,8 +4,9 @@ import ResizeObserver from 'resize-observer-polyfill';
 
 import { isNil, throttle } from "lodash-es";
 
-import { IChemJson, IDisplayOptions } from '@openchemistry/types';
+import { IChemJson, IDisplayOptions, IAtoms, IBonds } from '@openchemistry/types';
 import { composeDisplayOptions } from '@openchemistry/utils';
+import { validateChemJson, isChemJson } from '@openchemistry/utils';
 
 // import vtkFullScreenRenderWindow from 'vtk.js/Sources/Rendering/Misc/FullScreenRenderWindow';
 import vtk from 'vtk.js';
@@ -22,6 +23,7 @@ export class MoleculeVtkjs {
   @Prop() cjson: IChemJson;
   @Watch('cjson') cjsonHandler() {
     this.cjsonHasChanged = true;
+    this.cjsonData = null;
   }
 
   @Prop() options: IDisplayOptions;
@@ -34,7 +36,7 @@ export class MoleculeVtkjs {
 
   cjsonHasChanged: boolean = false;
 
-  animationInterval: number;
+  animationInterval: any;
 
   viewerContainer: HTMLElement;
   viewer: any;
@@ -45,18 +47,54 @@ export class MoleculeVtkjs {
   sphereActor: any;
   stickMapper: any;
   stickActor: any;
+  molecule: any;
 
   componentWillLoad() {
-    console.log('Component is about to be rendered');
-    let molecule = vtk.Common.DataModel.vtkMolecule.newInstance();
-    molecule.setAtoms(this.cjson.atoms);
-    molecule.setBonds(this.cjson.bonds);
+    console.log('MoleculeVtkjs is about to be rendered');
+    this.initVtkJs();
+  }
+
+  componentDidLoad() {
+    console.log('MoleculeVtkjs has been rendered');
+    this.viewer.setContainer(this.viewerContainer);
+    this.viewer.resize();
+
+    let throttledResize = throttle(() => {
+      if (!isNil(this.viewer)) {
+        this.viewer.resize();
+      }
+    }, 33);
+
+    const ro = new ResizeObserver(() => {
+      throttledResize();
+    });
+
+    ro.observe(this.viewerContainer);
+
+    this.startAnimation();
+  }
+
+  componentWillUpdate() {
+    console.log('MoleculeVtkjs did update');
+    if (this.cjsonHasChanged) {
+      this.updateMolecule();
+      this.renderer.resetCamera();
+      this.renderWindow.render();
+      this.cjsonHasChanged = false;
+    }
+
+    this.startAnimation();
+  }
+
+  initVtkJs() {
+    this.molecule = vtk.Common.DataModel.vtkMolecule.newInstance();
+    this.updateMolecule();
 
     this.viewer = vtk.Rendering.Misc.vtkGenericRenderWindow.newInstance();
     this.renderer = this.viewer.getRenderer();
     this.renderWindow = this.viewer.getRenderWindow();
     this.filter = vtk.Filters.General.vtkMoleculeToRepresentation.newInstance();
-    this.filter.setInputData(molecule);
+    this.filter.setInputData(this.molecule);
 
     this.sphereMapper = vtk.Rendering.Core.vtkSphereMapper.newInstance();
     this.sphereMapper.setInputConnection(this.filter.getOutputPort(0));
@@ -79,27 +117,101 @@ export class MoleculeVtkjs {
     this.renderWindow.render();
   }
 
-  componentDidLoad() {
-    console.log('Component has been rendered');
-    this.viewer.setContainer(this.viewerContainer);
-    this.viewer.resize();
+  cleanupVtkjs() {
+    this.molecule.delete();
+    this.viewer.delete();
+    this.filter.delete();
+    this.sphereMapper.delete();
+    this.sphereActor.delete();
+  }
 
-    let throttledResize = throttle(() => {
-      if (!isNil(this.viewer)) {
-        this.viewer.resize();
+  updateMolecule() {
+    let cjson = this.getCjson();
+    let atoms: IAtoms = !isNil(cjson) && !isNil(cjson.atoms) ? cjson.atoms : null;
+    let bonds: IBonds = !isNil(cjson) && !isNil(cjson.bonds) ? cjson.bonds : null;
+
+    this.molecule.setAtoms(atoms);
+    this.molecule.setBonds(bonds);
+  }
+
+  stopAnimation() {
+    // If an animation is playing, stop it
+    if (!isNil(this.animationInterval)) {
+      clearInterval(this.animationInterval);
+      // Place the atoms back to the starting positions
+      let cjson = this.getCjson();
+      let atoms: IAtoms = cjson.atoms ? cjson.atoms : null;
+      this.molecule.setAtoms(atoms);
+      this.renderWindow.render();
+      this.animationInterval = null;
+    }
+  }
+
+  startAnimation() {
+    // If an animation is playing, stop it before starting a new one
+    this.stopAnimation();
+    // Start an interval to play the normal mode animation
+    const cjson = this.getCjson();
+    const normalMode = this.getOptions().normalMode;
+    if (!isNil(cjson) && !isNil(cjson.vibrations) && !isNil(cjson.vibrations.eigenVectors) && normalMode.play) {
+      let modeIdx: number = normalMode.modeIdx;
+      if (modeIdx < 0) {
+        return;
       }
-    }, 33);
-
-    const ro = new ResizeObserver(() => {
-      throttledResize();
-    });
-
-    ro.observe(this.viewerContainer);
+      const eigenvector = cjson.vibrations.eigenVectors[modeIdx];
+      let frame: number = 1;
+      this.animationInterval = setInterval(() => {
+        let coords: number[] = [...cjson.atoms.coords['3d']];
+        let scale = normalMode.scale * Math.sin(2 * Math.PI * frame / normalMode.framesPerPeriod);
+        for (let i = 0; i < eigenvector.length; ++i) {
+          let dx = scale * eigenvector[i];
+          coords[i] += dx;
+        }
+        let atoms = {...cjson.atoms};
+        atoms.coords = {['3d']: coords};
+        this.molecule.setAtoms(atoms);
+        this.renderWindow.render();
+        frame++;
+      }, 1000 / (normalMode.framesPerPeriod * normalMode.periodsPerSecond));
+    }
   }
 
   componentDidUnload() {
     console.log('Component removed from the DOM');
-    this.viewer.delete();
+    this.cleanupVtkjs();
+  }
+
+  getCjson(): IChemJson {
+    if (isNil(this.cjsonData)) {
+      this.setCjson();
+    }
+    return this.cjsonData;
+  }
+
+  setCjson() {
+    if (isNil(this.cjson)) {
+      this.cjsonData = null;
+      return;
+    }
+    if (isChemJson(this.cjson)) {
+      this.cjsonData = this.cjson as IChemJson;
+    } else {
+      this.cjsonData = JSON.parse(this.cjson);
+    }
+    if (!validateChemJson(this.cjsonData)) {
+      this.cjsonData = null;
+    }
+  }
+
+  setOptions() {
+    this.optionsData = composeDisplayOptions(this.options);
+  }
+
+  getOptions() : IDisplayOptions {
+    if (isNil(this.optionsData)) {
+      this.setOptions();
+    }
+    return this.optionsData;
   }
 
   render() {
