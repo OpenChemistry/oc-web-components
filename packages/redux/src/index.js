4dec91cecba365c27fb75df3313b45254c64c3c6
Chris Harris <chris.harris@kitware.com>
2018-07-26 16:58:59 -0400

    fix: Export ducks

--- packages/redux/src/index.js
+++ packages/redux/src/index.js
@@ -1,30 +1,31 @@
-import molecules  from './ducks/molecules';
-import calculations  from './ducks/calculations';
-import users  from './ducks/users';
-import girder  from './ducks/girder';
-import app  from './ducks/app';
-import cumulus  from './ducks/cumulus';
-import nersc  from './ducks/nersc';
-import jupyterlab from './ducks/jupyterlab';
+import moleculesReducer, * as molecules  from './ducks/molecules';
+import calculationsReducer, * as calculations  from './ducks/calculations';
+import usersReducer, * as users  from './ducks/users';
+import girderReducer, * as girder  from './ducks/girder';
+import appReducer, * as app  from './ducks/app';
+import cumulusReducer, * as cumulus  from './ducks/cumulus';
+import nerscReducer, * as nersc  from './ducks/nersc';
+import jupyterlabReducer, * as jupyterlab from './ducks/jupyterlab';
+
 
 export const reducers = {
-  molecules,
-  calculations,
-  users,
-  girder,
-  app,
-  cumulus,
-  nersc,
-  jupyterlab
+    molecules: moleculesReducer,
+    calculations: calculationsReducer,
+    users: usersReducer,
+    girder: girderReducer,
+    app: appReducer,
+    cumulus: cumulusReducer,
+    nersc: nerscReducer,
+    jupyterlab: jupyterlabReducer
 };
 
 export { default as selectors } from './selectors';
 
-export * from './ducks/molecules';
-export * from './ducks/calculations';
-export * from './ducks/users';
-export * from './ducks/girder';
-export * from './ducks/cumulus';
-export * from './ducks/nersc';
-export * from './ducks/jupyterlab';
-export * from './ducks/app';
+export {molecules};
+export {calculations};
+export {users};
+export {girder};
+export {cumulus};
+export {nersc};
+export {jupyterlab};
+export {app};
