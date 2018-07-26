a297672828e4b042e628051eadb127ca280b98b2
Alessandro Genova <ales.genova@gmail.com>
2018-07-25 21:03:46 -0400

    Standalone component to render cjson data
    This components takes a cjson object as its prop,
    and renders a series of other component to visualize the data.
    Components include: molecule-vtks, vibrational-spectrum, molecule-menu

--- packages/molecule/src/components/molecule/molecule.tsx
+++ packages/molecule/src/components/molecule/molecule.tsx
@@ -1,14 +1,18 @@
-import { Component, Prop, Watch, Event, EventEmitter } from '@stencil/core';
+import { Component, Prop, State, Watch } from '@stencil/core';
 
 import { isNil } from "lodash-es";
 
-import { IChemJson } from '@openchemistry/types';
-import { isChemJson } from '@openchemistry/utils';
-import { composeDisplayOptions } from '@openchemistry/utils';
-import { IDisplayOptions } from '@openchemistry/types';
+import { IChemJson, IDisplayOptions } from '@openchemistry/types';
+import { isChemJson, validateChemJson, composeDisplayOptions } from '@openchemistry/utils';
 
-import '@openchemistry/molecule-moljs';
+import { Caffeine } from '@openchemistry/sample-data';
+
+import '@openchemistry/molecule-menu';
+import '@openchemistry/molecule-vtkjs';
 import '@openchemistry/vibrational-spectrum';
+import '@openchemistry/volume-controls';
+import '@ionic/core';
+import 'split-me';
 
 @Component({
   tag: 'oc-molecule',
@@ -16,21 +20,17 @@ import '@openchemistry/vibrational-spectrum';
 })
 export class Molecule {
 
-  // The chemical json object in iput
-  // Pure string fallback if used outside of JS or frameworks
-  @Prop() cjson: IChemJson | string;
-
-  @Prop() options: IDisplayOptions;
-  @Watch('options') optionsHandler(newValue: IDisplayOptions) {
-    this.optionsData = composeDisplayOptions(newValue);
+  // The chemical json object in input
+  @Prop() cjson: IChemJson;
+  @Watch('cjson')
+  cjsonHandler() {
+    this.cjsonData = null;
   }
 
-  @Event() barSelected: EventEmitter;
-
+  @State() options: IDisplayOptions = composeDisplayOptions({});
 
   cjsonData: IChemJson;
-
-  optionsData: IDisplayOptions;
+  cjsonHasChanged: boolean = false;
 
   componentWillLoad() {
     console.log('Molecule is about to be rendered');
@@ -54,51 +54,100 @@ export class Molecule {
     console.log('Molecule removed from the DOM');
   }
 
-  getCjson() : IChemJson {
-    if (isNil(this.cjson)) {
-      return null;
+  getCjson(): IChemJson {
+    if (isNil(this.cjsonData)) {
+      this.setCjson();
     }
-    if (isChemJson(this.cjson)) {
-      return this.cjson as IChemJson;
-    } else {
-      return JSON.parse(this.cjson);
-    }
+    // return this.cjsonData;
+    return Caffeine;
   }
 
-  onBarSelected(event: CustomEvent) {
-    if (event.type === "barSelected") {
-      // This is a "dumb" component, re-emit the event
-      this.barSelected.emit(event.detail);
+  setCjson() {
+    if (isNil(this.cjson)) {
+      this.cjsonData = null;
+      return;
+    }
+    if (isChemJson(this.cjson)) {
+      this.cjsonData = this.cjson as IChemJson;
+    } else {
+      this.cjsonData = JSON.parse(this.cjson);
+    }
+    if (!validateChemJson(this.cjsonData)) {
+      this.cjsonData = null;
     }
   }
 
   render() {
-    if (this.cjsonData && this.cjsonData.vibrations) {
-      return (
-        <div style={{width: "100%", heigth: "100%", display: "flex", position: "relative"}}>
-          <div style={{width: "50%", height: "40rem", position: "relative"}}>
-            <oc-molecule-moljs
-              cjson={this.cjsonData}
-              options={this.optionsData}
-            />
-          </div>
-          <div style={{width: "50%", height: "40rem", position: "relative"}}>
-            <oc-vibrational-spectrum
-              vibrations={this.cjsonData.vibrations}
-              onBarSelected={(ev) => this.onBarSelected(ev)}
-            />
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <div style={{width: "100%", heigth: "100%", display: "flex", position: "relative"}}>         
-          <oc-molecule-moljs
-            cjson={this.cjsonData}
-            options={this.optionsData}
+    const cjson = this.getCjson();
+    const hasVolume = !!cjson && !!cjson.cube;
+    const hasSpectrum = !!cjson && !!cjson.vibrations && !!cjson.vibrations.frequencies;
+
+    const nModes = hasSpectrum ? cjson.vibrations.frequencies.length : -1;
+    
+    const splitN = hasSpectrum ? 2 : 1;
+    const splitSizes = hasSpectrum ? "0.4, 0.6" : "1";
+
+    hasVolume;
+    nModes;
+
+    return (
+      <div class='main-container'>
+        <split-me n={splitN} sizes={splitSizes}>
+          <oc-molecule-vtkjs
+            slot='0'
+            cjson={cjson}
+            options={this.options}
           />
+          {hasSpectrum &&
+          <oc-vibrational-spectrum
+            slot='1'
+            vibrations={cjson.vibrations}
+          />
+          }
+        </split-me>
+        {/* { (hasSpectrum || hasVolume) && */}
+        <div class='menu-container'>
+          <oc-molecule-menu-popup>
+            <oc-molecule-menu
+              nModes={nModes}
+              hasVolume={true}
+
+              // ref={wc(
+              //   // Events
+              //   {
+              //     scaleValueChanged: (e)=>{this.onAmplitude(e.detail);},
+              //     isoValueChanged: (e) => {this.onIsoScale(e.detail);},
+              //     normalModeChanged: (e) => {this.onModeChange(e.detail);},
+              //     playChanged: (e) => {this.onPlayToggled(e.detail);},
+              //     opacitiesChanged: (e) => {this.onOpacitiesChanged(e.detail);},
+              //     visibilityChanged: (e) => {this.onVisibilityChanged(e.detail);},
+              //     colorMapChanged: (e) => {this.onColorMapChanged(e.detail);}
+              //   },
+              //   // Props
+              //   {
+              //     nModes: nModes,
+              //     iMode: Math.min(animation.modeIdx, nModes - 1),
+              //     scaleValue: animation.scale,
+              //     play: animation.play,
+              //     hasVolume: hasVolume,
+              //     isoValue: this.state.isoSurfaces[0].value,
+              //     volumeOptions: {
+              //       colors: this.state.volume.colors,
+              //       opacity: this.state.volume.opacity,
+              //       opacityScalarValue: this.state.volume.opacityScalarValue,
+              //       range: this.state.volume.range,
+              //       histograms: histograms
+              //     },
+              //     visibilityOptions: this.state.visibility,
+              //     colorMaps: ['Red Yellow Blue', 'Viridis', 'Plasma', 'Gray'],
+              //     activeMap: this.state.volume.mapName
+              //   })
+              // }
+              ></oc-molecule-menu>
+          </oc-molecule-menu-popup>
         </div>
-      )
-    }
+        {/* } */}
+      </div>
+    )
   }
 }
