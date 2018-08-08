cacec819e11db146b94dcd7232a9f43ec99beddb
Alessandro Genova <alessandro.genova@kitware.com>
2018-08-08 15:30:39 -0400

    fix(sagas): Fix naming clash in calculations saga

--- packages/sagas/src/calculations.js
+++ packages/sagas/src/calculations.js
@@ -2,20 +2,20 @@ import { select, put, call, all, takeEvery} from 'redux-saga/effects'
 import { isNil } from 'lodash-es'
 
 import { file } from '@openchemistry/rest'
-import { calculations } from '@openchemistry/redux';
+import { calculations as calculationsRedux } from '@openchemistry/redux'
 import { selectors } from '@openchemistry/redux';
 
 export function* loadCalculationNotebooks(action) {
   try {
     const { calculationId } = action.payload;
-    yield put(calculations.requestCalculationNotebooks(calculationId));
+    yield put(calculationsRedux.requestCalculationNotebooks(calculationId));
 
     const calculations = yield select(selectors.calculations.getCalculationsById);
     const calculation = calculations[calculationId];
     const notebooks = calculation.notebooks;
 
     if (isNil(notebooks)) {
-      put(calculations.receiveCalculationNotebooks(calculationId, []));
+      put(calculationsRedux.receiveCalculationNotebooks(calculationId, []));
       return;
     }
 
@@ -26,14 +26,14 @@ export function* loadCalculationNotebooks(action) {
 
     const files = yield all(calls);
 
-    yield put(calculations.receiveCalculationNotebooks(calculationId, files));
+    yield put(calculationsRedux.receiveCalculationNotebooks(calculationId, files));
   }
   catch(error) {
-    yield put(calculations.requestCalculationNotebooks(error));
+    yield put(calculationsRedux.requestCalculationNotebooks(error));
   }
 }
 
 export function* watchLoadCalculationNotebooks() {
-  yield takeEvery(calculations.LOAD_CALCULATION_NOTEBOOKS, loadCalculationNotebooks);
+  yield takeEvery(calculationsRedux.LOAD_CALCULATION_NOTEBOOKS, loadCalculationNotebooks);
 }
 
