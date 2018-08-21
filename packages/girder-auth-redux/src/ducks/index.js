c7d781353bbdb982620ade9bf37f8d2b574b4122
Alessandro Genova <alessandro.genova@kitware.com>
2018-08-21 18:10:16 -0400

    Move girder redux/rest/saga auth code into a reusable package

---
+++ packages/girder-auth-redux/src/ducks/index.js
@@ -0,0 +1,173 @@
+import { createAction, handleActions } from 'redux-actions';
+
+const PREFIX = '@@girder-auth-redux/';
+
+const initialState = {
+  token: null,
+  oauth: {
+    providers: {},
+    enabled: false,
+  },
+  me: null,
+  authenticating: false,
+  error: null,
+  ui: {
+    showLoginOptions: false,
+    girder: {
+      show: false
+    },
+    // nersc: {
+    //   show: false
+    // }
+  }
+};
+
+// Selectors
+export const getMe = (state) => state.me;
+export const getToken = (state) => state.token;
+export const getOauthProviders = (state) => state.oauth.providers;
+export const isAuthenticating = state => state.authenticating;
+export const isAuthenticated = state => state.token && !state.authenticating;
+export const isOauthEnabled = state => state.oauth.enabled;
+export const getShowLoginOptions = (state) => state.ui.showLoginOptions;
+export const getShowGirderLogin = (state) => state.ui.girder.show;
+
+// Actions
+
+export const AUTHENTICATE = PREFIX + 'AUTHENTICATE';
+
+export const SET_TOKEN = PREFIX + 'SET_TOKEN';
+export const SET_AUTHENTICATING = PREFIX + 'SET_AUTHENTICATING';
+export const SET_ME = PREFIX + 'SET_ME';
+
+export const INVALIDATE_TOKEN = PREFIX + 'INVALIDATE_TOKEN';
+export const REQUEST_INVALIDATE_TOKEN = PREFIX + 'REQUEST_INVALIDATE_TOKEN';
+
+export const USERNAME_LOGIN = PREFIX + 'USERNAME_LOGIN';
+
+export const LOAD_ME = PREFIX + 'LOAD_ME';
+export const REQUEST_ME = PREFIX + 'REQUEST_ME';
+
+export const LOAD_OAUTH_PROVIDERS = PREFIX + 'LOAD_OAUTH_PROVIDERS';
+export const REQUEST_OAUTH_PROVIDERS = PREFIX + 'REQUEST_OAUTH_PROVIDERS';
+export const RECEIVE_OAUTH_PROVIDERS = PREFIX + 'RECEIVE_OAUTH_PROVIDERS';
+
+export const TEST_OAUTH_ENABLED = 'TEST_OAUTH_ENABLED';
+export const SET_OAUTH_ENABLED = 'SET_OAUTH_ENABLED';
+
+export const SHOW_LOGIN_OPTIONS = PREFIX + 'SHOW_LOGIN_OPTIONS';
+export const SHOW_GIRDER_LOGIN = PREFIX + 'SHOW_GIRDER_LOGIN';
+// export const SHOW_NERSC_LOGIN = PREFIX + 'SHOW_NERSC_LOGIN';
+
+// Action creators
+export const authenticate = createAction(AUTHENTICATE);
+export const setAuthenticating = createAction(SET_AUTHENTICATING);
+
+export const invalidateToken = createAction(INVALIDATE_TOKEN);
+export const requestInvalidateToken = createAction(REQUEST_INVALIDATE_TOKEN);
+
+export const loadOauthProviders = createAction(LOAD_OAUTH_PROVIDERS);
+export const requestOauthProviders = createAction(REQUEST_OAUTH_PROVIDERS);
+export const receiveOauthProviders = createAction(RECEIVE_OAUTH_PROVIDERS);
+export const testOauthEnabled = createAction(TEST_OAUTH_ENABLED);
+export const setOauthEnabled = createAction(SET_OAUTH_ENABLED);
+
+export const loadMe = createAction(LOAD_ME);
+export const requestMe = createAction(REQUEST_ME);
+export const receiveMe = createAction(SET_ME);
+
+export const usernameLogin = createAction(USERNAME_LOGIN);
+
+export const setToken = createAction(SET_TOKEN);
+export const setMe = createAction(SET_ME);
+
+export const showLoginOptions = createAction(SHOW_LOGIN_OPTIONS);
+export const showGirderLogin = createAction(SHOW_GIRDER_LOGIN);
+// export const showNerscLogin = createAction(SHOW_NERSC_LOGIN);
+
+
+
+// Reducer
+
+// For some reason the reducer created by redux-actions' handleActions() doesn't
+// seem to work. For the sake of time, implementing a plain reducer instead.
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case SET_TOKEN: {
+      const token = action.payload;
+      // const providers = null;
+      // const oauth = {
+      //     providers
+      // }
+      return {...state, token};
+    }
+
+    case SET_AUTHENTICATING: {
+      const authenticating = action.payload;
+      return {...state, authenticating};
+    }
+    
+    case SET_ME: {
+      const me = action.payload
+      return {...state, me};
+    }
+    
+    case REQUEST_INVALIDATE_TOKEN: {
+      if (action.error) {
+        return {...state, error: action.payload.error};
+      }
+      else {
+        return {...state,  error:null };
+      }
+    }
+      
+    case REQUEST_OAUTH_PROVIDERS: {
+      if (action.error) {
+        return {...state, error: action.payload.error};
+      }
+      else {
+        return {...state,  error:null };
+      }
+    }
+
+    case SET_OAUTH_ENABLED: {
+      const enabled = action.payload;
+      const oauth = {
+        ...state.oauth,
+        enabled
+      }
+      return {...state, oauth};
+    }
+
+    case RECEIVE_OAUTH_PROVIDERS: {
+      const providers = action.payload;
+      const oauth = {
+          ...state.oauth,
+          providers
+      }
+      return {...state, oauth};
+    }
+
+    case SHOW_LOGIN_OPTIONS: {
+      const show = action.payload;
+      const ui = {...state.ui};
+      ui.showLoginOptions = show;
+      return {...state, ui}
+    }
+
+    case SHOW_GIRDER_LOGIN: {
+      const show = action.payload;
+      const ui = {...state.ui};
+      ui.girder = {show};
+      return {...state, ui}
+    }
+      
+    default:  {
+      return state;
+    }
+  }
+}
+
+export default reducer;
+
