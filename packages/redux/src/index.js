843cdc9242f0349bbf23476d1793c4d5f2661ba4
Alessandro Genova <alessandro.genova@kitware.com>
2018-11-07 17:15:10 -0500

    Fix dependencies

--- packages/redux/src/index.js
+++ packages/redux/src/index.js
@@ -1,6 +1,5 @@
 import moleculesReducer, * as molecules  from './ducks/molecules';
 import calculationsReducer, * as calculations  from './ducks/calculations';
-import usersReducer, * as users  from './ducks/users';
 import girderReducer, * as girder  from './ducks/girder';
 import appReducer, * as app  from './ducks/app';
 import cumulusReducer, * as cumulus  from './ducks/cumulus';
@@ -11,7 +10,6 @@ import jupyterlabReducer, * as jupyterlab from './ducks/jupyterlab';
 export const reducers = {
     molecules: moleculesReducer,
     calculations: calculationsReducer,
-    users: usersReducer,
     girder: girderReducer,
     app: appReducer,
     cumulus: cumulusReducer,
@@ -23,7 +21,6 @@ export { default as selectors } from './selectors';
 
 export {molecules};
 export {calculations};
-export {users};
 export {girder};
 export {cumulus};
 export {nersc};
