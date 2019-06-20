90f0bd490fda3891b046dfbca59aa2be98552e0b
Brianna Major <brianna.major@kitware.com>
2019-06-20 11:40:54 -0400

    Added component to display group information

---
+++ packages/girder-ui/src/admin/components/groups/index.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { Table,
+	 TableHead,
+	 TableBody,
+	 TableRow,
+	 TableCell,
+         Link, Paper } from '@material-ui/core';
+
+class Groups extends Component {
+  render() {
+    const { listOfGroups } = this.props;
+    return(
+      <Paper>
+      <Table>
+	<TableHead>
+          <TableRow>
+	    <TableCell>Group Name</TableCell>
+	    <TableCell>Decription</TableCell>
+	    <TableCell>Date Created</TableCell>
+	    <TableCell>Last Updated</TableCell>
+	    <TableCell>Status</TableCell>
+	    <TableCell>Delete Group</TableCell>  
+	  </TableRow>
+	</TableHead>
+        <TableBody>
+	  {listOfGroups.map((group) => (
+	    <TableRow key={group._id}>
+	      <TableCell>
+                <Link component="button"
+	          group={group}
+	          onClick={(e) => {this.props.handleClick(group)}}>
+		  { group.name }
+                </Link>	      
+	      </TableCell>
+	      <TableCell>{ group.description }</TableCell>
+	      <TableCell>{ group.created.slice(0,10) }</TableCell>
+	      <TableCell>{ group.updated.slice(0,10)
+			   + " at "
+			   + group.updated.slice(11, 19) }</TableCell>
+	      <TableCell>{ group.public ? "Public" : "Private" }</TableCell>
+	      <TableCell>
+		<Link component="button"
+	          onClick={(e) => {
+		    this.props.handleDelete(group._id)}}>
+		  Remove
+	        </Link>
+	      </TableCell>
+	    </TableRow>
+	  ))}
+	</TableBody>
+      </Table>
+      </Paper>
+    );
+  }
+}
+
+Groups.propTypes = {
+    listOfGroups: PropTypes.array
+}
+
+Groups.defaultProps = {
+    listOfGroups: []
+}
+
+export default Groups;
