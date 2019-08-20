8ae9789ab5844e5c5997c3bb524d824388ff5f70
bnmajor <51238406+bnmajor@users.noreply.github.com>
2019-08-20 11:01:29 -0400

    Fix tab formatting
    Signed-off-by: Brianna Major <brianna.major@kitware.com>

--- packages/girder-ui/src/admin/components/members/index.js
+++ packages/girder-ui/src/admin/components/members/index.js
@@ -1,5 +1,5 @@
 import {
-	Link, Paper, Table, TableBody, TableCell, TableHead, TableRow, Typography
+  Link, Paper, Table, TableBody, TableCell, TableHead, TableRow, Typography
 } from '@material-ui/core';
 import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
 
@@ -7,61 +7,61 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 class Members extends Component {
-	render() {
-		const {group, listOfMembers, search} = this.props;
-		return(
-			<div style={{margin:'10px'}}>
-				<Typography style={{textAlign:'center'}} variant='h4' gutterBottom>
-					{group.name}
-				</Typography>
-				<Paper>
-					<Table>
-						<TableHead>
-							<TableRow>
-								<TableCell>First Name</TableCell>
-								<TableCell>Last Name</TableCell>
-								<TableCell>Username</TableCell>
-								<TableCell>Date Created</TableCell>
-								<TableCell>Admin</TableCell>
-								<TableCell>Remove User</TableCell>
-							</TableRow>
-						</TableHead>
-						<TableBody>
-							{listOfMembers.map((user) => (
-								<TableRow key={ user._id }>
-									<TableCell>{user.firstName}</TableCell>
-									<TableCell>{user.lastName}</TableCell>
-									<TableCell>{user.login}</TableCell>
-									<TableCell>{user.created.slice(0,10)}</TableCell>
-									<TableCell>{user.admin ? 'Yes' : 'No'}</TableCell>
-									<TableCell>
-										<Link
-											component='button'
-											onClick={(e) => 
-												{this.props.handleDelete(user._id, group, search)}
-											}
-										>
-											<DeleteOutlineIcon />
-										</Link>
-									</TableCell>
-								</TableRow>
-							))}
-						</TableBody>
-					</Table>
-				</Paper>
-			</div>      
-		);
-	}
+  render() {
+    const {group, listOfMembers, search} = this.props;
+    return(
+      <div style={{margin:'10px'}}>
+        <Typography style={{textAlign:'center'}} variant='h4' gutterBottom>
+          {group.name}
+        </Typography>
+        <Paper>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>First Name</TableCell>
+                <TableCell>Last Name</TableCell>
+                <TableCell>Username</TableCell>
+                <TableCell>Date Created</TableCell>
+                <TableCell>Admin</TableCell>
+                <TableCell>Remove User</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {listOfMembers.map((user) => (
+                <TableRow key={ user._id }>
+                  <TableCell>{user.firstName}</TableCell>
+                  <TableCell>{user.lastName}</TableCell>
+                  <TableCell>{user.login}</TableCell>
+                  <TableCell>{user.created.slice(0,10)}</TableCell>
+                  <TableCell>{user.admin ? 'Yes' : 'No'}</TableCell>
+                  <TableCell>
+                    <Link
+                      component='button'
+                      onClick={(e) =>
+                        {this.props.handleDelete(user._id, group, search)}
+                      }
+                    >
+                      <DeleteOutlineIcon />
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
+      </div>
+    );
+  }
 }
 
 Members.propTypes = {
-	listOfMembers: PropTypes.array,
-	group: PropTypes.object
+  listOfMembers: PropTypes.array,
+  group: PropTypes.object
 }
 
 Members.defaultProps = {
-	listOfMembers: [],
-	group: {}
+  listOfMembers: [],
+  group: {}
 }
 
 export default Members;
--- packages/girder-ui/src/admin/components/users/index.js
+++ packages/girder-ui/src/admin/components/users/index.js
@@ -1,5 +1,5 @@
 import {
-	Link, Paper, Table, TableBody, TableCell, TableHead, TableRow, Typography
+  Link, Paper, Table, TableBody, TableCell, TableHead, TableRow, Typography
 } from '@material-ui/core';
 import PersonAddIcon from '@material-ui/icons/PersonAdd';
 
@@ -7,54 +7,54 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 class Users extends Component {
-	render() {
-		const {group, possibleUsers, showUsers, search}=this.props;
-		const query=search ? search : 'all available users'
-		return(
-			<Paper style={{display:'table', padding:'5px', margin:'auto'}}>
-				<Typography style={{textAlign: 'center'}} variant='subtitle1' gutterBottom>
-					Showing search results for {query}:
-				</Typography>
-				<Table style={{width:'auto', display:'block', margin:'auto'}}>
-					<TableHead>
-						<TableRow>
-							<TableCell>Name</TableCell>
-							<TableCell>Username</TableCell>
-							<TableCell>Add User</TableCell>
-						</TableRow>
-					</TableHead>
-					<TableBody>
-						{possibleUsers.map((person) => (
-							<TableRow key={person._id}>
-								<TableCell>
-									{person.firstName+' '+person.lastName}
-								</TableCell>
-								<TableCell>{person.login}</TableCell>
-								<TableCell>
-									<Link
-										component='button'
-										onClick={(e) =>
-											{this.props.handleAdd(person._id, group)}
-										}
-									>
-										<PersonAddIcon />
-									</Link>
-								</TableCell>
-							</TableRow>
-						))}
-					</TableBody>
-				</Table>
-			</Paper>
-		);
-	}
+  render() {
+    const {group, possibleUsers, showUsers, search}=this.props;
+    const query=search ? search : 'all available users'
+    return(
+      <Paper style={{display:'table', padding:'5px', margin:'auto'}}>
+        <Typography style={{textAlign: 'center'}} variant='subtitle1' gutterBottom>
+          Showing search results for {query}:
+        </Typography>
+        <Table style={{width:'auto', display:'block', margin:'auto'}}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Username</TableCell>
+              <TableCell>Add User</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {possibleUsers.map((person) => (
+              <TableRow key={person._id}>
+                <TableCell>
+                  {person.firstName+' '+person.lastName}
+                </TableCell>
+                <TableCell>{person.login}</TableCell>
+                <TableCell>
+                  <Link
+                    component='button'
+                    onClick={(e) =>
+                      {this.props.handleAdd(person._id, group)}
+                    }
+                  >
+                    <PersonAddIcon />
+                  </Link>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
 
 Users.propTypes = {
-	possibleUsers: PropTypes.array
+  possibleUsers: PropTypes.array
 }
 
 Users.defaultProps = {
-	possibleUsers: []
+  possibleUsers: []
 }
 
 export default Users;
