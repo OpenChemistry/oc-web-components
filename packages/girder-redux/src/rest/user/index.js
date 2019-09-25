27ae8aaed30a18ca436ad34aad875658fda60489
Brianna Major <brianna.major@kitware.com>
2019-09-25 11:41:24 -0400

    Add rest calls to girder API key endpoints
    Signed-off-by: Brianna Major <brianna.major@kitware.com>

--- packages/girder-redux/src/rest/user/index.js
+++ packages/girder-redux/src/rest/user/index.js
@@ -18,7 +18,7 @@ export function updateUserInformation(id, values) {
 
 export function twitterLogin(userId, mediaId) {
   const data = new FormData();
-    data.set('twitter', mediaId);
+  data.set('twitter', mediaId);
 
   return girderClient()
   .post(`user/${userId}/twitter`, data)
@@ -27,7 +27,7 @@ export function twitterLogin(userId, mediaId) {
 
 export function orcidLogin(userId, mediaId) {
   const data = new FormData();
-    data.set('orcid', mediaId);
+  data.set('orcid', mediaId);
 
   return girderClient()
   .post(`user/${userId}/orcid`, data)
@@ -41,5 +41,39 @@ export function twitterId(id) {
 
 export function orcidId(id) {
   return girderClient().get(`user/${id}/orcid`)
-  .then(response => response.data);
+  .then(response => response.data)
+}
+
+export function getApiKeys(apiKey) {
+  return girderClient().get(`api_key`)
+  .then(response => response.data)
+}
+
+export function deleteApiKey(id) {
+  return girderClient().delete(`api_key/${id}`)
+  .then(response => response.data)
+}
+
+export function editApiKey(key) {
+  const { active, scope, tokenDuration, name } = key;
+  const data = new FormData();
+  data.set('active', active);
+  data.set('scope', JSON.stringify(scope));
+  data.set('tokenDuration', tokenDuration ? tokenDuration : '');
+  data.set('name', name);
+
+  return girderClient().put(`api_key/${key._id}`, data)
+  .then(response => response.data)
+}
+
+export function createApiKey(key) {
+  const { active, scope, tokenDuration, name } = key;
+  const data = new FormData();
+  data.set('active', active);
+  data.set('scope', JSON.stringify(scope));
+  data.set('tokenDuration', tokenDuration ? tokenDuration : '');
+  data.set('name', name);
+
+  return girderClient().post(`api_key`, data)
+  .then(response => response.data)
 }
